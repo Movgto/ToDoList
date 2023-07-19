b86cd07af4dfccae152fbc27fee7116877ae8ae0
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-07-19 10:47:36 -0600

    Update linters setup and fix

--- src/modules/listStorage.js
+++ src/modules/listStorage.js
@@ -1,20 +1,20 @@
 class List {
     static tasks = [
-        {
-            index: 0,
-            description: 'Cook breakfast',
-            completed: true
-        },
-        {
-            index: 1,
-            description: 'Clean the kitchen',
-            completed: false
-        },
-        {
-            index: 2,
-            description: 'Grocery shopping',
-            completed: false
-        }
+      {
+        index: 0,
+        description: 'Cook breakfast',
+        completed: true,
+      },
+      {
+        index: 1,
+        description: 'Clean the kitchen',
+        completed: false,
+      },
+      {
+        index: 2,
+        description: 'Grocery shopping',
+        completed: false,
+      },
     ];
 }
 
